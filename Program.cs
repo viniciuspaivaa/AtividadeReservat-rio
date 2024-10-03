269bd5a6a00d83e6bbc3a1f2cfa4cd9600a2017f
Vinicius <vinicius.paiva2@outlook.com>
2024-10-03 11:27:31 -0300

    Implementando funcionanmento de bóias

--- Program.cs
+++ Program.cs
@@ -16,11 +16,12 @@
         while(!erro)
         {
             //Reservatório vazio
-            if(reserva <= minReserva && caixa <= maxCaixa)
+            if(reserva <= minReserva && caixa < maxCaixa)
             {
                 bomba = false;
                 torneira = true;
                 reserva += 4;
+                resolucao = "nenhuma";
             }
             //Reservatório ideal, caixa vazia
             else if(reserva >= minReserva && reserva < maxReserva && caixa < maxCaixa)
@@ -29,13 +30,15 @@
                 torneira = true;
                 reserva += 2;
                 caixa += 2;
+                resolucao = "A";
             }
             //Reservatório ideal, caixa cheia
-            else if(reserva <= maxReserva && caixa >= maxCaixa)
+            else if(reserva < maxReserva && caixa == maxCaixa)
             {
                 bomba = false;
                 torneira = true;
                 reserva += 4;
+                resolucao = "A e C";
             }
             //Reservatório cheio, caixa vazia
             else if(reserva >= maxReserva && caixa < maxCaixa)
@@ -44,16 +47,19 @@
                 torneira = false;
                 reserva -= 2;
                 caixa += 2;
+                resolucao = "A";
             }
             //Reservatório cheio, caixa cheia
             else if(reserva >= maxCaixa && caixa >= maxCaixa)
             {
                 bomba = false;
                 torneira = false;
+                resolucao = "A, B e C";
             }
             else
             {
                 erro = true;
+                resolucao = "erro";
             }
 
 
@@ -67,16 +73,23 @@
             }
             else if(caixa == maxCaixa)
             {
-                Console.WriteLine($"Caixa d'água: {caixa}/30\nReservatório: {reserva}/100\n\nBomba: {onOffBomb}\nTorneira: {onOffTorn}\nBóias: {resolucao}");
+                Console.WriteLine($"Caixa d'água: {caixa}/30\nReservatório: {reserva}/100\n\nBomba: {onOffBomb}\nTorneira: {onOffTorn}\nBóias ativadas: {resolucao}");
                 Console.Write("Esvaziando a caixa d'água...");
                 caixa = 0;
                 Thread.Sleep(4000);
             }
             else
             {
-                Console.Write($"Caixa d'água: {caixa}/30\nReservatório: {reserva}/100\n\nBomba: {onOffBomb}\nTorneira: {onOffTorn}");
+                Console.Write($"Caixa d'água: {caixa}/30\nReservatório: {reserva}/100\n\nBomba: {onOffBomb}\nTorneira: {onOffTorn}\nBóias ativadas: {resolucao}");
             }
-            Thread.Sleep(1000);
+            var key = Console.ReadKey(true);
+            
+            // Verifica se a tecla pressionada foi Enter
+            if (key.Key == ConsoleKey.Enter)
+            {
+                continue; // Continua o loop
+            }
+            //Thread.Sleep(3000);
         }
     }
 }
